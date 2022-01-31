13a61114cd3b15807642b8956145dc92eaffb61a
slavak86 <55182559+slavak86@users.noreply.github.com>
2022-01-31 13:13:08 +0500

    Delete

--- tic-tac-toe/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.tictactoe {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.tictactoe to javafx.fxml;
-    exports com.example.tictactoe;
-}
