da1c5387488f8861e5aa0488d89659561344a65f
slavak86 <55182559+slavak86@users.noreply.github.com>
2022-01-31 13:12:37 +0500

    Delete

--- src/main/java/ru/gb/lesson2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.lesson2;
-
-public class MyArraySizeException extends Exception {
-
-    MyArraySizeException(int requiredSize) {
-        super(String.format("Размерность массива отличается от %d%n", requiredSize));
-    }
-}
